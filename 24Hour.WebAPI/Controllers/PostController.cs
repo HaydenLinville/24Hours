266a467ac8c79e66d391c1f5acf5099d3d7e59ba
Hayden Linville <hayden.linville@gmail.com>
2022-04-04 20:59:00 -0400

    post controller, postservice, and post models added.

---
+++ 24Hour.WebAPI/Controllers/PostController.cs
@@ -0,0 +1,77 @@
+﻿using _24Hour.Models;
+using _24Hour.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _24Hour.WebAPI.Controllers
+{
+    [Authorize]
+    public class PostController : ApiController
+    {
+
+
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var post = postService.GetPosts();
+            return Ok(post);
+        }
+
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.CreatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());//getuserid was for microsoft.aspnet.identity
+            var postService = new PostService(userId);
+            return postService;
+        }
+
+
+        public IHttpActionResult Get(int id)
+        {
+            PostService postService = CreatePostService();
+            var post = postService.GetPostById(id);
+            return Ok();
+        }
+
+        public IHttpActionResult Put(PostEdit post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.UpdatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreatePostService();
+
+            if (!service.DeletePost(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+    }
+}
